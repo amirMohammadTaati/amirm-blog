e91a2b5a927bd8e93a51cd1ced87096d77aae46e
mamad <mamad@mamad.mamad>
2022-03-05 15:35:49 +0330

    Apollo provider added

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,12 +1,16 @@
 import { ChakraProvider } from "@chakra-ui/react";
-import "../styles/globals.css";
 import type { AppProps } from "next/app";
+import { ApolloProvider } from "@apollo/client";
+import client from "../apollo";
+import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }: AppProps) {
   return (
-    <ChakraProvider>
-      <Component {...pageProps} />
-    </ChakraProvider>
+    <ApolloProvider client={client}>
+      <ChakraProvider>
+        <Component {...pageProps} />
+      </ChakraProvider>
+    </ApolloProvider>
   );
 }
 
