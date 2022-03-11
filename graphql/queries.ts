043879f969dc0927bbabdfc7712d45f3ec9ead30
mamad <mamad@mamad.mamad>
2022-03-11 11:49:57 +0330

    I don't ask for image url anymore

--- graphql/queries.ts
+++ graphql/queries.ts
@@ -5,6 +5,18 @@ export const GET_ALL_POSTS = gql`
     posts {
       title
       description
+      slug
+      _id
+    }
+  }
+`;
+
+export const GET_POST = gql`
+  query posts($slug: String) {
+    post(slug: "post2") {
+      title
+      description
+      content
       image {
         url
       }
