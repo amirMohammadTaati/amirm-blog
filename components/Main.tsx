15b389eadb651947bf7fc82ca766362eebb57ff3
mamad <mamad@mamad.mamad>
2022-03-11 14:18:36 +0330

    Main updated

--- components/Main.tsx
+++ components/Main.tsx
@@ -1,3 +1,26 @@
-export default function Main() {
-  return <main></main>;
+import { Box } from "@chakra-ui/react";
+import { PostArray } from "../lib/types";
+import PostCard from "./PostCard";
+
+export default function Main({ posts }: PostArray) {
+  return (
+    <Box
+      w="70%"
+      h="100vh"
+      float="left"
+      padding="50px 170px"
+      paddingBottom="75px"
+    >
+      {posts.map((post) => (
+        <PostCard
+          title={post.title}
+          description={post.description}
+          slug={post.slug}
+          _id={post._id}
+          key={post._id}
+          createdAt={post.createdAt}
+        />
+      ))}
+    </Box>
+  );
 }
