a834b4a5cc684a389aae4238b72f3d2eab0393a3
mamad <mamad@mamad.mamad>
2022-03-11 11:49:37 +0330

    having image removed from post type

--- lib/types.ts
+++ lib/types.ts
@@ -1,14 +1,9 @@
-interface Image {
-  url: string;
-}
-
 export interface Post {
   title: string;
   description: string;
   slug: string;
   _id: string;
   key: string;
-  image: Image;
 }
 
 export interface PostArray {
